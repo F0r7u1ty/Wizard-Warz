059f92a85b58679929571c4a7226661e3e22032e
JB <jbockmann@cedarville.edu>
2025-10-22 16:02:22 -0400

    Basic movement added and playerLook
    Still need to add shift for "teleport" and e to interact, other than that every movement mechanic is in the game.

---
+++ Unity Project/Assets/Scripts/InputManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.LowLevel;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+    private PlayerMotor motor;
+    private PlayerLook look;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+        motor = GetComponent<PlayerMotor>();
+        //anytime onFoot.jump performed, use callback context to call motor.Jump()
+        onFoot.Jump.performed += ctx => motor.Jump();
+        look = GetComponent<PlayerLook>();
+        onFoot.Crouch.performed += ctx => motor.Crouch();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //tell playerMotor to move using value from input action
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
+    }
+
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
---
+++ Unity Project/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    public float speed = 5f;
+    public float gravity = -9.8f;
+    private bool isGrounded;
+    public float jumpHeight = 1.3f;
+    private bool lerpCrouch;
+    private bool crouching;
+    private float crouchTimer;
+    private float crouchSpeed = 11f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+        if (lerpCrouch)
+        {
+            crouchTimer += Time.deltaTime;
+            float p = crouchTimer / 1;
+            p *= p;
+            if (crouching)
+            {
+                controller.height = Mathf.Lerp(controller.height, 1, p);
+            }
+            else
+            {
+                controller.height = Mathf.Lerp(controller.height, 2, p);
+            }
+            if (p > 1)
+            {
+                lerpCrouch = false;
+                crouchTimer = 0f;
+            }
+        }
+    }
+
+    //receive inputs for InputManager.cs and apply them to character controller
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+        float currentSpeed = crouching ? crouchSpeed : speed;
+        Debug.Log("currentSpeed: " + currentSpeed);
+        controller.Move(transform.TransformDirection(moveDirection) * currentSpeed * Time.deltaTime);
+        playerVelocity.y += gravity * Time.deltaTime;
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+        controller.Move(playerVelocity * Time.deltaTime);
+        //Debug.Log(playerVelocity.y);
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+    }
+
+    public void Crouch()
+    {
+        crouching = !crouching;
+        crouchTimer = 0;
+        lerpCrouch = true;
+    }
+}
