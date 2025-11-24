7f70bfc1a8a789401f98509429df3698e0a4e499
Jewl <165347751+Zjewl@users.noreply.github.com>
2025-11-24 14:28:57 -0500

    Health and Mana UI update
    Health and Mana UI are now graphic bars instead of text, the text is still underneath for testing purposes, you can disable the graphic bars that overlay them if you want to view precise numbers in testing.

--- Unity Project/Assets/Scripts/Basic_UI.cs
+++ Unity Project/Assets/Scripts/Basic_UI.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using TMPro;
 using System.Runtime.CompilerServices;
+using UnityEngine.UI;
 
 public class Basic_UI : MonoBehaviour
 {
 
     //objects for showing particular stats
-    public Transform healthUI;
-    public Transform manaUI;
+    public Transform healthUI_text;
+    public Transform manaUI_text;
+    public Image healthUI_barbar;
+    public Image manaUI_barbar;
     //text component of objects
     private TextMeshProUGUI textMesh;
 
@@ -19,13 +22,15 @@ public class Basic_UI : MonoBehaviour
 
     void UpdateHealth(int health)
     {
-        textMesh = healthUI.GetComponent<TextMeshProUGUI>();
+        textMesh = healthUI_text.GetComponent<TextMeshProUGUI>();
         textMesh.text = "Health: " + health;
+        healthUI_barbar.fillAmount = (float)health / GameData.MAX_PLAYER_HEALTH;
     }
     void UpdateMana(int mana)
     {
-        textMesh = manaUI.GetComponent<TextMeshProUGUI>();
+        textMesh = manaUI_text.GetComponent<TextMeshProUGUI>();
         textMesh.text = "Mana: " + mana;
+        manaUI_barbar.fillAmount = (float)mana / GameData.MAX_PLAYER_MANA;
     }
 }
 
